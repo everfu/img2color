fbb458436fc3c78c5bae5ea6ae2dd5da0a31e04a
Jose wan <wleelw@sondy.eu.org>
2024-01-21 15:27:20 +0800

    Update img2color.go

--- api/img2color.go
+++ api/img2color.go
@@ -1,20 +1,25 @@
 package handler
 
 import (
+	"bytes"
 	"context"
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"github.com/chai2010/webp"
 	"github.com/disintegration/imaging"
-	"github.com/fogleman/gg"
 	"github.com/go-redis/redis/v8"
 	"github.com/joho/godotenv"
 	"image"
+	"image/draw"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
+	"sync"
+	"time"
 )
 
 type Response struct {
@@ -119,23 +124,56 @@ func setColorToKV(imgURL string, color string) {
 	}
 }
 
+var bufferPool = sync.Pool{
+	New: func() interface{} {
+		return new(bytes.Buffer)
+	},
+}
+
 func getColorFromImageURL(imgURL string) (string, error) {
-	resp, err := http.Get(imgURL)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, imgURL, nil)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return "", err
 	}
 	defer resp.Body.Close()
 
-	img, _, err := image.Decode(resp.Body)
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
 	}
 
+	buf := bufferPool.Get().(*bytes.Buffer)
+	defer bufferPool.Put(buf)
+	buf.Reset()
+	buf.Write(bodyBytes)
+
+	var img image.Image
+	img, _, err = image.Decode(buf)
+	if err != nil {
+		// If standard image decoding fails, try to decode as WebP
+		img, err = webp.Decode(buf)
+		if err != nil {
+			return "", err
+		}
+	}
+
 	img = imaging.Resize(img, 1, 1, imaging.Box)
-	dc := gg.NewContext(1, 1)
-	dc.DrawImage(img, 0, 0)
-	rVal, g, b, _ := dc.Image().At(0, 0).RGBA()
+	dc := image.NewRGBA(image.Rect(0, 0, 1, 1))
+	draw.Draw(dc, dc.Bounds(), img, img.Bounds().Min, draw.Src)
+	rVal, g, b, _ := dc.At(0, 0).RGBA()
 	color := fmt.Sprintf("#%02X%02X%02X", uint8(rVal>>8), uint8(g>>8), uint8(b>>8))
 
 	return color, nil
+}
+func main() {
+	http.HandleFunc("/", Handler)
+	http.ListenAndServe(":8080", nil)
 }
