18529c7b9677aaf177f9efa19760ee0819ae77c2
Jose wan <wleelw@sondy.eu.org>
2024-01-21 16:02:21 +0800

    Update img2color.go

--- api/img2color.go
+++ api/img2color.go
@@ -51,6 +51,10 @@ func init() {
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
 	if !checkReferer(r) {
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 		return
